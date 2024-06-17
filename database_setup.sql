63365b0ad88326390d6ec9bb5f72e6aaf67a8286
Dabelu <dokigbo@ndc.nasa.gov>
2024-06-17 11:52:51 -0400

    pushing my code to Respitory

---
+++ database_setup.sql
@@ -0,0 +1,14 @@
+-- Create the main table for storing the check file data
+CREATE TABLE data_sources (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    source_name TEXT UNIQUE
+);
+
+-- Create a table to store the status records
+CREATE TABLE status_records (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    data_source_id INTEGER,
+    status INTEGER,
+    status_date DATE,
+    FOREIGN KEY (data_source_id) REFERENCES data_sources (id)
+);
