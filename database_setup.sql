a6b171947fe164d5dbd95f40be2d4851151242b8
Dabelu <dokigbo@ndc.nasa.gov>
2024-06-18 13:27:33 -0400

    This helped my tables to show up when i run .tables

--- database_setup.sql
+++ database_setup.sql
@@ -1,12 +1,12 @@
 -- Create the main table for storing the check file data
-CREATE TABLE data_sources (
+CREATE TABLE IF NOT EXISTS data_sources (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     source_name TEXT UNIQUE
 );
 
 
 -- Create a table to store the status records
-CREATE TABLE status_records (
+CREATE TABLE IF NOT EXISTS status_records (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     data_source_id INTEGER,
     status INTEGER,
