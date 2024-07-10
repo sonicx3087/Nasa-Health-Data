dd032f656c49c40f29b29f96e13068e755c397c9
Dabelu <dokigbo@ndc.nasa.gov>
2024-07-10 01:26:41 -0400

    ADDED DASHES

--- populate_database.py
+++ populate_database.py
@@ -46,7 +46,7 @@ for filename in os.listdir(directory):
             print(f"CSV file loaded successfully: {filename}")
             # Extract date from the filename (assuming the format is consistent)
             date_str = filename.split('_')[3][:8]  # Extract the date part
-            check_date = datetime.strptime(date_str, '%Y%m%d').strftime('%Y%m%d')
+            check_date = datetime.strptime(date_str, '%Y%m%d').strftime('%Y-%m-%d')
             insert_check_file_data(df, check_date)
         except FileNotFoundError:
             print(f"File not found: {file_path}")
